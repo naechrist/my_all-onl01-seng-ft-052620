764935129691a565f576c6bcf36e4013cddb91aa
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-30 01:28:31 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,13 @@ def my_all?(collection)
   block_return_values = []
 i = 0 
 while i < collection.length 
-yield(collection[i])
+block_return_values << yield(collection[i])
 i = i + 1 
 yield(collection[i])
+end
+if block_return_values.include?(false)
+  false
+else
+  TRUE
 end
 end
